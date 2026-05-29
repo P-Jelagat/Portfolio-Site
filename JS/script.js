6b8804cc18f85c8ee83534100014e512e2aeec14
Patience Jelagat <patiencejelagatt@gmail.com>
2026-05-29 16:52:39 +0300

    Added chsnges with active class reassigning upon each click of a link

--- JS/script.js
+++ JS/script.js
@@ -2,18 +2,22 @@ const navLinks = document.querySelectorAll('.navLink');
 const underline = document.querySelector('.underline');
 
 navLinks.forEach( (navLink) => {
+   navLink.addEventListener('click', ()=>{
+    removeActiveClass();
+    navLink.classList.add("active");
+    moveUnderline(navLink);
+   })
 
-    navLink.classList.toggle("active");
-
-    navLink.addEventListener('click', () =>{
-        moveUnderline(navLink);
-        changePage();
-
-    } );
 });
 
-
+const removeActiveClass = () => {
+    navLinks.forEach((navLink) => {
+        navLink.classList.remove("active");
+    })
+}
 function moveUnderline(navLink){
     underline.style.width = `${navLink.offsetWidth}px`;
     underline.style.transform= `translateX(${navLink.offsetLeft}px)`;
 }
+
+moveUnderline(document.querySelector('.active'));
