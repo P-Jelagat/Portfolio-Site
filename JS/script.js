cbd7797b8d0cd38c9ebce8c9d3e47132e695ab42
Patience Jelagat <patiencejelagatt@gmail.com>
2026-05-29 19:20:11 +0300

    First Complete Version

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,15 @@
 const navLinks = document.querySelectorAll('.navLink');
 const underline = document.querySelector('.underline');
 
-navLinks.forEach( (navLink) => {
-   navLink.addEventListener('click', ()=>{
+
+// THIS IS FOR THE UNDERLINE MOVEMENT ALONG THE NAVBAR
+navLinks.forEach( (navLink, index) => {
+   navLink.addEventListener('click', (e)=>{
+    e.preventDefault();
     removeActiveClass();
     navLink.classList.add("active");
     moveUnderline(navLink);
+    changePage(index);
    })
 
 });
@@ -21,3 +25,40 @@ function moveUnderline(navLink){
 }
 
 moveUnderline(document.querySelector('.active'));
+
+
+// THIS IS FOR THE PAGE TRANSITIONS
+
+const pages = document.querySelectorAll('.page');
+let currentIndex = 0;
+
+function changePage(newIndex){
+
+    const currentPage = pages[currentIndex];
+    const nextPage = pages[newIndex];
+
+    currentPage.classList.remove('activePage');
+    nextPage.classList.add('activePage');
+
+    const nextContent = nextPage.querySelector('.pageContent');
+    nextContent.classList.remove('fromLeft', 'fromRight', 'showPage');
+    
+    if(newIndex > currentIndex){
+        nextContent.classList.add('fromRight');
+    }else{
+        nextContent.classList.add('fromLeft');
+    }
+
+    void nextContent.offsetWidth;
+
+    nextContent.classList.add('showPage');
+     currentIndex = newIndex;
+
+}
+
+
+/* INITIAL UNDERLINE POSITION */
+
+moveUnderline(
+    document.querySelector('.active')
+);
